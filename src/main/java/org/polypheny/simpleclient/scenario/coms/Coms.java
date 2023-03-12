caa561cd099edf85799cc263538f0861e4e5d486
datomo <dave.lengw@gmail.com>
2023-03-12 23:41:20 +0100

    added simulation boilerplate

---
+++ src/main/java/org/polypheny/simpleclient/scenario/coms/Coms.java
@@ -0,0 +1,99 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019-3/11/23, 11:17 AM The Polypheny Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.polypheny.simpleclient.scenario.coms;
+
+import java.io.File;
+import java.util.Properties;
+import java.util.Random;
+import lombok.extern.slf4j.Slf4j;
+import org.polypheny.simpleclient.QueryMode;
+import org.polypheny.simpleclient.executor.Executor;
+import org.polypheny.simpleclient.executor.Executor.ExecutorFactory;
+import org.polypheny.simpleclient.executor.ExecutorException;
+import org.polypheny.simpleclient.main.CsvWriter;
+import org.polypheny.simpleclient.main.ProgressReporter;
+import org.polypheny.simpleclient.scenario.Scenario;
+
+@Slf4j
+public class Coms extends Scenario {
+
+    public static final String NAMESPACE = "coms";
+
+    private final Random random;
+    private final ComsConfig config;
+
+
+    protected Coms( ComsConfig config, ExecutorFactory executorFactory, boolean commitAfterEveryQuery, boolean dumpQueryList, QueryMode queryMode ) {
+        super( executorFactory, commitAfterEveryQuery, dumpQueryList, queryMode );
+        this.random = new Random( config.seed );
+        this.config = config;
+    }
+
+
+    @Override
+    public void createSchema( boolean includingKeys ) {
+        log.info( "Generating data..." );
+        Executor executor = executorFactory.createExecutorInstance( null, NAMESPACE );
+        org.polypheny.simpleclient.scenario.coms.DataGenerator dataGenerator = new DataGenerator();
+        try {
+            dataGenerator.generateData();
+        } catch ( ExecutorException e ) {
+            throw new RuntimeException( "Exception while generating data", e );
+        } finally {
+            commitAndCloseExecutor( executor );
+        }
+    }
+
+
+    @Override
+    public void generateData( ProgressReporter progressReporter ) {
+
+    }
+
+
+    @Override
+    public long execute( ProgressReporter progressReporter, CsvWriter csvWriter, File outputDirectory, int numberOfThreads ) {
+        return 0;
+    }
+
+
+    @Override
+    public void warmUp( ProgressReporter progressReporter ) {
+
+    }
+
+
+    @Override
+    public void analyze( Properties properties, File outputDirectory ) {
+
+    }
+
+
+    @Override
+    public int getNumberOfInsertThreads() {
+        return 0;
+    }
+
+}
---
+++ src/main/java/org/polypheny/simpleclient/scenario/coms/simulation/NetworkGenerator.java
@@ -0,0 +1,212 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019-3/11/23, 11:48 AM The Polypheny Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.polypheny.simpleclient.scenario.coms.simulation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.polypheny.simpleclient.scenario.coms.ComsConfig;
+import org.polypheny.simpleclient.scenario.coms.simulation.Graph.Edge;
+import org.polypheny.simpleclient.scenario.coms.simulation.Graph.Node;
+
+public class NetworkGenerator {
+
+    public final ComsConfig config;
+    private final Random random;
+    private final Network network;
+
+
+    /**
+     * Network architecture relies on a low amount of main architecture
+     * with a low connectivity
+     * Depending on size of users a amount of Switches and APs is deploy.
+     *
+     * @param config
+     */
+    public NetworkGenerator( ComsConfig config ) {
+        this.config = config;
+        this.random = new Random( config.seed );
+
+        // generate network (simulation)
+        // -> generate structure of network -> nodes
+        // -> generate parameters per nodes
+        // -> generate configs depending on type of node
+        // -> generate logs?
+
+        this.network = new Network( random, config.networkScale );
+
+    }
+
+
+    @Value
+    public static class Network {
+
+        public static long SERVERS = 5;
+
+        public static long CLIENTS = 100;
+
+        public static double OS_DISTRIBUTION = 0.7;
+
+        public static long MOBILES = 400;
+
+        public static long APS = 100;
+
+        public static long SWITCHES = 5;
+        Random random;
+        List<Server> servers = new ArrayList<>();
+        List<Switch> switches = new ArrayList<>();
+
+        List<AP> aps = new ArrayList<>();
+        List<IoT> ioTs = new ArrayList<>();
+
+        List<Mobile> mobiles = new ArrayList<>();
+
+        List<PC> pcs = new ArrayList<>();
+
+        List<Mac> macs = new ArrayList<>();
+        int scale;
+
+
+        public Network( Random random, int scale ) {
+            this.random = random;
+            this.scale = scale;
+
+            generateObject( SERVERS, () -> servers.add( new Server( random ) ) );
+            generateObject( SWITCHES, () -> switches.add( new Switch( random ) ) );
+            generateObject( APS, () -> aps.add( new AP( random ) ) );
+
+            generateObject( CLIENTS * OS_DISTRIBUTION, () -> pcs.add( new PC( random ) ) );
+            generateObject( CLIENTS * (1 - OS_DISTRIBUTION), () -> macs.add( new Mac( random ) ) );
+
+            generateObject( MOBILES * OS_DISTRIBUTION, () -> mobiles.add( new Mobile( random ) ) );
+            generateObject( MOBILES * (1 - OS_DISTRIBUTION), () -> ioTs.add( new IoT( random ) ) );
+        }
+
+
+        private void generateObject( double amount, Runnable runnable ) {
+            for ( long i = 0; i < amount * this.scale; i++ ) {
+                runnable.run();
+            }
+        }
+
+
+        Graph toGraph() {
+            return null;
+        }
+
+    }
+
+    //// Backbone
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class Server extends Node {
+
+        Random random;
+
+    }
+
+    //// Distributors
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class Switch extends Node {
+
+        Random random;
+
+    }
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class AP extends Node {
+
+        Random random;
+
+    }
+
+    //// Devices
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class IoT extends Node {
+
+        Random random;
+
+    }
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class Mobile extends Node {
+
+        Random random;
+
+    }
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class PC extends Node {
+
+        Random random;
+
+    }
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class Mac extends Node {
+
+        Random random;
+
+    }
+
+    //// Connections
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class Lan extends Edge {
+
+        Random random;
+
+    }
+
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    public static class WLan extends Edge {
+
+        Random random;
+
+    }
+
+}
