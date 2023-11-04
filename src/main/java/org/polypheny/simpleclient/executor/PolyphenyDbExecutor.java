2c47ac3a82a7433f8b1345762796ad0598ba4159
Marco Vogt <marco.vogt@unibas.ch>
2023-11-04 11:19:48 +0100

    Push purge plugins setting to Polypheny Control

--- src/main/java/org/polypheny/simpleclient/executor/PolyphenyDbExecutor.java
+++ src/main/java/org/polypheny/simpleclient/executor/PolyphenyDbExecutor.java
@@ -545,6 +545,7 @@ public interface PolyphenyDbExecutor extends Executor {
                 conf.put( "pcrtl.buildmode", "pdb" );
             }
             conf.put( "pcrtl.clean.mode", "branchChange" );
+            conf.put( "pcrtl.plugins.purge", "afterBuilding" );
             String args = "";
             if ( resetCatalog ) {
                 args += "-resetCatalog ";
