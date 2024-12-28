cadc4eeaa2d0be91b94d50cdda1a7be1226a8f40
Marco Vogt <marco.vogt@unibas.ch>
2024-12-28 12:36:42 +0100

    Fix license header

--- src/main/java/org/polypheny/simpleclient/main/DockerLauncher.java
+++ src/main/java/org/polypheny/simpleclient/main/DockerLauncher.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2019-12/6/24, 9:27 AM The Polypheny Project
+ * Copyright (c) 2019-2024 The Polypheny Project
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"), to deal
